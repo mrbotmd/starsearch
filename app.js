cbd5abac37514499a2dc8a7c766b686e76ce00f9
mrbotmd <mr.bot.md@gmail.com>
2020-02-11 00:47:23 +0600

    render logic done next css

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-// (function () {
 "use strict";
-
-// import actorConstructor from "./actorConstructor.js";
-
 const searchResultField = document.querySelector(".search__result");
 const optionSelector = document.querySelector(".search__option-selector");
 const searchBtn = document.querySelector(".search__btn");
@@ -27,10 +23,9 @@ let apiCall = url => {
           count: Math.ceil(data.count / 10)
         };
         return output;
-      } else {
-        console.log(data);
-        return data;
-      }
+      } else if (data.count == 0) {
+        return;
+      } else return data;
     })
     .catch(err => {
       if (err) {
@@ -53,7 +48,10 @@ let searchQuery = data => {
     // searchParams.append("", name);
     // console.log(`${rootURL}${optionSelector.value}/?search${searchParams}`);
     return apiCall(
-      `${rootURL}${optionSelector.value}/?search=${queryObject.value}`
+      `${rootURL}${optionSelector.value}/?search=${queryObject.value.replace(
+        /\s/g,
+        "-"
+      )}`
     );
   }
   // else if (data.toElement.tagName = "a" && data.toElement.dataset.apilink.includes("page")) {
@@ -76,8 +74,8 @@ let buildPaginator = data => {
   let buildNPPages = data => {
     let nextPage = document.createElement("a");
     let prevPage = document.createElement("a");
-    nextPage.setAttribute("href", "#");
-    // page.setAttribute("href", "#");
+    nextPage.setAttribute("href", "#/");
+    prevPage.setAttribute("href", "#/");
     if (data.next && data.previous) {
       prevPage.textContent = "Prev Page";
       nextPage.textContent = "Next Page";
@@ -154,7 +152,7 @@ let showLinks = async data => {
   let parentElement = data.toElement.parentElement;
   let element = data.toElement;
   let linkData = await call(data);
-  console.log(linkData);
+  console.log(linkData.dataset);
   let ul = document.createElement("ul");
   linkData.forEach(item => {
     let linkListItem = document.createElement("a");
@@ -162,7 +160,6 @@ let showLinks = async data => {
       linkListItem.textContent = item.title;
       linkListItem.setAttribute("data-apilink", item);
       let linkLi = links(item.url, item.title);
-      // console.log(link.firstChild);
       linkLi.firstChild.removeEventListener("click", showLinks);
       linkLi.firstChild.addEventListener("click", buildNewPage, { once: true });
       ul.appendChild(linkLi);
@@ -172,6 +169,7 @@ let showLinks = async data => {
       linkLi.firstChild.addEventListener("click", buildNewPage, { once: true });
       linkListItem.textContent = item.name;
       linkListItem.setAttribute("data-apilink", item);
+      console.log(linkLi);
       ul.appendChild(linkLi);
     }
   });
@@ -179,43 +177,100 @@ let showLinks = async data => {
 };
 
 let links = (option, optionName) => {
+  // if (option.length) {
   let listItem = document.createElement("li");
   let linkTag = document.createElement("a");
-  linkTag.setAttribute("href", "#");
+  console.log(option);
+  linkTag.setAttribute("href", "#/");
   linkTag.setAttribute("data-apilink", option);
   linkTag.addEventListener("click", showLinks, { once: true });
   linkTag.textContent = optionName;
   listItem.appendChild(linkTag);
   return listItem;
+  // }
+  // return;
 };
 
 let buildResultForm = data => {
   console.log(data);
-  // let info = [];
-  // info.push(data);
-  // console.log(info);
-  // if (data.output[0].url.includes("people")) {
-  // console.log(data.output[0].url.includes("people"));
-  buildPeopleResultForm(data);
-  // } else if (data.output[0].url.includes("films")) {
-  // }
-  return data;
+  let typeCheck = (obj, objURL) => {
+    if (objURL.includes("people")) {
+      if (obj instanceof Array) {
+        console.log(obj);
+        obj.forEach(item => {
+          buildCharacterForm(item);
+        });
+      } else {
+        buildCharacterForm(obj);
+      }
+    } else if (objURL.includes("films")) {
+      if (obj instanceof Array) {
+        console.log(obj);
+        obj.forEach(item => {
+          buildFilmForm(item);
+        });
+      } else {
+        buildFilmForm(obj);
+      }
+    } else if (objURL.includes("planets")) {
+      if (obj instanceof Array) {
+        console.log(obj);
+        obj.forEach(item => {
+          buildPlanetForm(item);
+        });
+      } else {
+        buildPlanetForm(obj);
+      }
+    } else if (objURL.includes("species")) {
+      if (obj instanceof Array) {
+        console.log(obj);
+        obj.forEach(item => {
+          buildSpeciesForm(item);
+        });
+      } else {
+        buildSpeciesForm(obj);
+      }
+    } else if (objURL.includes("vehicles")) {
+      if (obj instanceof Array) {
+        console.log(obj);
+        obj.forEach(item => {
+          buildVehiclesForm(item);
+        });
+      } else {
+        buildVehiclesForm(obj);
+      }
+    } else if (objURL.includes("starships")) {
+      if (obj instanceof Array) {
+        console.log(obj);
+        obj.forEach(item => {
+          buildStarshipsForm(item);
+        });
+      } else {
+        buildStarshipsForm(obj);
+      }
+    }
+  };
+  if (data.output === undefined) {
+    let item = data;
+    let itemURL = data.url;
+    return typeCheck(item, itemURL);
+  } else {
+    let item = data.output;
+    let itemURL = data.output[0].url;
+    return typeCheck(item, itemURL);
+  }
 };
 
-let buildPeopleResultForm = data => {
-  console.log(data);
-  // console.log(data.output);
-  let item;
-  let resultList = document.createElement("ul");
-  let build = character => {
+let buildCharacterForm = data => {
+  let article = document.createElement("article");
+  let buildCharacter = character => {
+    this.character = character;
     let img = document.createElement("img");
     let randomImg = "https://source.unsplash.com/random/150x150";
     let p = document.createElement("p");
     let linksList = document.createElement("ul");
-    linksList.textContent = "linksList";
     linksList.setAttribute("class", "link-list");
     let li = document.createElement("li");
-    let article = document.createElement("article");
     let articleHeader = document.createElement("header");
     let h1 = document.createElement("h1");
     let div = document.createElement("div");
@@ -238,38 +293,300 @@ let buildPeopleResultForm = data => {
         Birth Year: ${character.birth_year} <br />
         Gender: ${character.gender} <br />`;
     img.setAttribute("src", randomImg);
-    resultList.appendChild(li);
-    li.appendChild(article);
     articleHeader.appendChild(h1);
     article.appendChild(articleHeader);
     article.appendChild(div);
     div.appendChild(img);
     div.appendChild(p);
     div.appendChild(linksList);
-    linksList.appendChild(links(character.films, filmsList));
-    linksList.appendChild(links(character.homeworld, homeworldList));
-    linksList.appendChild(links(character.species, speciesList));
-    linksList.appendChild(links(character.vehicles, vehiclesList));
-    linksList.appendChild(links(character.starships, starshipsList));
+    if (character.films.length != 0) {
+      linksList.appendChild(links(character.films, filmsList));
+    }
+    if (character.homeworld.length != 0) {
+      linksList.appendChild(links(character.homeworld, homeworldList));
+    }
+    if (character.species.length != 0) {
+      linksList.appendChild(links(character.species, speciesList));
+    }
+    if (character.vehicles.length != 0) {
+      linksList.appendChild(links(character.vehicles, vehiclesList));
+    }
+    if (character.starships.length != 0) {
+      linksList.appendChild(links(character.starships, starshipsList));
+    }
   };
 
-  if (data.output === undefined) {
-    item = data;
-    build(item);
-  } else {
-    item = data.output;
-    item.forEach(character => {
-      build(character);
-    });
-  }
-  searchResultField.appendChild(resultList);
+  buildCharacter(data);
+  searchResultField.appendChild(article);
+};
+
+let buildFilmForm = data => {
+  let article = document.createElement("article");
+  let buildFilm = films => {
+    this.film = films;
+    let img = document.createElement("img");
+    let randomImg = "https://source.unsplash.com/random/150x150";
+    let p = document.createElement("p");
+    let linksList = document.createElement("ul");
+    linksList.setAttribute("class", "link-list");
+    let li = document.createElement("li");
+    let articleHeader = document.createElement("header");
+    let h1 = document.createElement("h1");
+    let div = document.createElement("div");
+    let charactersList = Object.keys(film)[6];
+    let planetsList = Object.keys(film)[7];
+    let starshipsList = Object.keys(film)[8];
+    let vehiclesList = Object.keys(film)[9];
+    let speciesList = Object.keys(film)[10];
+
+    h1.textContent = film.title;
+    h1.setAttribute("data-apilink", film.url);
+    h1.addEventListener("click", buildNewPage);
+    div.setAttribute("class", "container--flex");
+    p.innerHTML = `
+        Title: ${film.title} <br />
+        Episode: ${film.episode_id} <br />
+        Opening crawl: ${film.opening_crawl} <br />
+        Director: ${film.director} <br />
+        Producer: ${film.producer} <br />
+        Release Date: ${film.release_date} <br />`;
+    img.setAttribute("src", randomImg);
+    articleHeader.appendChild(h1);
+    article.appendChild(articleHeader);
+    article.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(p);
+    div.appendChild(linksList);
+    if (film.characters.length != 0) {
+      linksList.appendChild(links(film.characters, charactersList));
+    }
+    if (film.planets.length != 0) {
+      linksList.appendChild(links(film.planets, planetsList));
+    }
+    if (film.species.length != 0) {
+      linksList.appendChild(links(film.species, speciesList));
+    }
+    if (film.vehicles.length != 0) {
+      linksList.appendChild(links(film.vehicles, vehiclesList));
+    }
+    if (film.starships.length != 0) {
+      linksList.appendChild(links(film.starships, starshipsList));
+    }
+  };
+
+  buildFilm(data);
+  searchResultField.appendChild(article);
+};
+
+let buildPlanetForm = data => {
+  let article = document.createElement("article");
+  let buildPlanet = planets => {
+    this.planet = planets;
+    let img = document.createElement("img");
+    let randomImg = "https://source.unsplash.com/random/150x150";
+    let p = document.createElement("p");
+    let linksList = document.createElement("ul");
+    linksList.setAttribute("class", "link-list");
+    let li = document.createElement("li");
+    let articleHeader = document.createElement("header");
+    let h1 = document.createElement("h1");
+    let div = document.createElement("div");
+    let residentsList = Object.keys(planet)[9];
+    let filmsList = Object.keys(planet)[10];
+
+    h1.textContent = planet.name;
+    h1.setAttribute("data-apilink", planet.url);
+    h1.addEventListener("click", buildNewPage);
+    div.setAttribute("class", "container--flex");
+    p.innerHTML = `
+        Title: ${planet.name} <br />
+        Rotation Period: ${planet.rotation_period} <br />
+        Orbital Period: ${planet.orbital_period} <br />
+        Diameter: ${planet.diameter} <br />
+        Climate: ${planet.climate} <br />
+        Gravity: ${planet.gravity} <br />
+        Terrain: ${planet.terrain} <br />
+        Surface Water: ${planet.surface_water} <br />
+        Population: ${planet.population} <br />`;
+    img.setAttribute("src", randomImg);
+    articleHeader.appendChild(h1);
+    article.appendChild(articleHeader);
+    article.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(p);
+    div.appendChild(linksList);
+    if (planet.residents.length != 0) {
+      linksList.appendChild(links(planet.residents, residentsList));
+    }
+    if (planet.films.length != 0) {
+      linksList.appendChild(links(planet.films, filmsList));
+    }
+  };
+
+  buildPlanet(data);
+  searchResultField.appendChild(article);
+};
+
+let buildSpeciesForm = data => {
+  let article = document.createElement("article");
+  let buildSpecies = species => {
+    this.species = species;
+    let img = document.createElement("img");
+    let randomImg = "https://source.unsplash.com/random/150x150";
+    let p = document.createElement("p");
+    let linksList = document.createElement("ul");
+    linksList.setAttribute("class", "link-list");
+    let li = document.createElement("li");
+    let articleHeader = document.createElement("header");
+    let h1 = document.createElement("h1");
+    let div = document.createElement("div");
+    let homeworldList = Object.keys(species)[8];
+    let peopleList = Object.keys(species)[10];
+    let filmsList = Object.keys(species)[11];
+
+    h1.textContent = species.name;
+    h1.setAttribute("data-apilink", species.url);
+    h1.addEventListener("click", buildNewPage);
+    div.setAttribute("class", "container--flex");
+    p.innerHTML = `
+        Name: ${species.name} <br />
+        Classification: ${species.classification} <br />
+        Designation: ${species.designation} <br />
+        Average Height: ${species.average_height} <br />
+        Skin Colors: ${species.skin_colors} <br />
+        Hair Colors: ${species.hair_colors} <br />
+        Eye Colors: ${species.eye_colors} <br />
+        Average Lifespan: ${species.average_lifespan} <br />
+        Language: ${species.language} <br />`;
+    img.setAttribute("src", randomImg);
+    articleHeader.appendChild(h1);
+    article.appendChild(articleHeader);
+    article.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(p);
+    div.appendChild(linksList);
+    if (species.homeworld.length != 0) {
+      linksList.appendChild(links(species.homeworld, homeworldList));
+    }
+    if (species.people.length != 0) {
+      linksList.appendChild(links(species.people, peopleList));
+    }
+    if (species.films.length != 0) {
+      linksList.appendChild(links(species.films, filmsList));
+    }
+  };
+
+  buildSpecies(data);
+  searchResultField.appendChild(article);
+};
+
+let buildVehiclesForm = data => {
+  let article = document.createElement("article");
+  let buildVehicle = vehicles => {
+    this.vehicle = vehicles;
+    let img = document.createElement("img");
+    let randomImg = "https://source.unsplash.com/random/150x150";
+    let p = document.createElement("p");
+    let linksList = document.createElement("ul");
+    linksList.setAttribute("class", "link-list");
+    let li = document.createElement("li");
+    let articleHeader = document.createElement("header");
+    let h1 = document.createElement("h1");
+    let div = document.createElement("div");
+    let pilotsList = Object.keys(vehicle)[11];
+    let filmsList = Object.keys(vehicle)[12];
+
+    h1.textContent = vehicle.name;
+    h1.setAttribute("data-apilink", vehicle.url);
+    h1.addEventListener("click", buildNewPage);
+    div.setAttribute("class", "container--flex");
+    p.innerHTML = `
+        Title: ${vehicle.name} <br />
+        Model: ${vehicle.model} <br />
+        Manufacturer: ${vehicle.manufacturer} <br />
+        Cost In Creditst: ${vehicle.cost_in_credits} <br />
+        Length: ${vehicle.length} <br />
+        Max Atmosphering Speed: ${vehicle.max_atmosphering_speed} <br />
+        Crew: ${vehicle.crew} <br />
+        Passengers: ${vehicle.passengers} <br />
+        Cargo Capacity: ${vehicle.cargo_capacity} <br />
+        Consumables: ${vehicle.consumables} <br />
+        Vehicle Class: ${vehicle.vehicle_class} <br />`;
+    img.setAttribute("src", randomImg);
+    articleHeader.appendChild(h1);
+    article.appendChild(articleHeader);
+    article.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(p);
+    div.appendChild(linksList);
+    if (vehicle.pilots.length != 0) {
+      linksList.appendChild(links(vehicle.pilots, pilotsList));
+    }
+    if (vehicle.films.length != 0) {
+      linksList.appendChild(links(vehicle.films, filmsList));
+    }
+  };
+
+  buildVehicle(data);
+  searchResultField.appendChild(article);
+};
+
+let buildStarshipsForm = data => {
+  let article = document.createElement("article");
+  let buildStarship = starships => {
+    this.starship = starships;
+    let img = document.createElement("img");
+    let randomImg = "https://source.unsplash.com/random/150x150";
+    let p = document.createElement("p");
+    let linksList = document.createElement("ul");
+    linksList.setAttribute("class", "link-list");
+    let li = document.createElement("li");
+    let articleHeader = document.createElement("header");
+    let h1 = document.createElement("h1");
+    let div = document.createElement("div");
+    let pilotsList = Object.keys(starship)[13];
+    let filmsList = Object.keys(starship)[14];
+
+    h1.textContent = starship.name;
+    h1.setAttribute("data-apilink", starship.url);
+    h1.addEventListener("click", buildNewPage);
+    div.setAttribute("class", "container--flex");
+    p.innerHTML = `
+        Title: ${starship.name} <br />
+        Model: ${starship.model} <br />
+        Manufacturer: ${starship.manufacturer} <br />
+        Cost In Creditst: ${starship.cost_in_credits} <br />
+        Length: ${starship.length} <br />
+        Max Atmosphering Speed: ${starship.max_atmosphering_speed} <br />
+        Crew: ${starship.crew} <br />
+        Passengers: ${starship.passengers} <br />
+        Cargo Capacity: ${starship.cargo_capacity} <br />
+        Consumables: ${starship.consumables} <br />
+        Hyperdrive Rating: ${starship.hyperdrive_rating} <br />
+        MGLT: ${starship.MGLT} <br />
+        Starship Class: ${starship.starship_class} <br />`;
+    img.setAttribute("src", randomImg);
+    articleHeader.appendChild(h1);
+    article.appendChild(articleHeader);
+    article.appendChild(div);
+    div.appendChild(img);
+    div.appendChild(p);
+    div.appendChild(linksList);
+    if (starship.pilots.length != 0) {
+      linksList.appendChild(links(starship.pilots, pilotsList));
+    }
+    if (starship.films.length != 0) {
+      linksList.appendChild(links(starship.films, filmsList));
+    }
+  };
+
+  buildStarship(data);
+  searchResultField.appendChild(article);
 };
 
 async function search() {
   let result = await searchQuery();
-  console.log(result);
   build(result);
-  // console.log(url.urlList);
 }
 
 let build = data => {
@@ -283,4 +600,3 @@ const clear = () => {
     searchResultField.removeChild(searchResultField.firstChild);
   }
 };
-// })()
